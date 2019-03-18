9ac5e9444dc6271e1f5ac6ddffe27d8567016017
tywilliam <tyresedev@gmail.com>
2019-03-18 04:30:52 -0400

    Tcreate, filter and list of todos

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+            <h3>Add Todo</h3>
+            <div class="add">
+                <form @submit="onSubmit">
+                    <input type="text" name="" v-model="title" placeholder="Add Todo..." id="">
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        ...mapActions(['addTodo']),
+        onSubmit(e) {
+            e.preventDefault();
+            this.addTodo(this.title);
+        }
+    }
+}
+</script>
+
+<style>
+ form {
+     display: flex;
+ }
+ input[type="text"] {
+     flex: 10;
+     padding: 10px;
+     border: 1px solid #41b883 solid;
+     cursor: pointer;
+ }
+ input[type="submit"] {
+     flex: 2;
+     background:#41b883;
+     color:#fff;
+     border:1px solid #41b883 solid;
+     cursor: pointer;
+ }
+
+</style>
